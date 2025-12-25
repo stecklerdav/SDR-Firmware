1afb1e1e53972ff5bd5763b9730ffd6291b689de
David Steckler <stecklerdav@hotmail.com>
2025-12-25 16:49:57 +0000

    Remove local fpga workspace and clean project tree

--- fpga/pluto/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-ROOT=$SCRIPT_DIR/..
-
-cd $ROOT
-vivado -mode batch -source tcl/system_project.tcl
-
--- fpga/pluto/scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rm -rf *.xpr *.jou *.log *.str \
-       *.cache *.runs *.gen *.ip_user_files
-
